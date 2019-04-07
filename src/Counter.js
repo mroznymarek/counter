21273e39448ec80897da6d2e56789b8754826271
Marek Mróz <marekmroz@wp.pl>
2019-04-07 22:01:46 +0200

    fixed problems add local storage

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 
-class Counter extends React.Component {
+class Counter extends Component {
     state = {
-        number: this.props.number
+        number: JSON.parse(localStorage.getItem('counter')) || this.props.number
+    }
+
+    saveNumber(){
+        localStorage.setItem('counter', JSON.stringify(this.state.number))
     }
 
     adding1() {
-        this.setState({ number: this.state.number + 1 })
+        this.setState({number: this.state.number + 1})
     }
 
     substract1() {
@@ -22,40 +26,42 @@ class Counter extends React.Component {
         this.setState({ number: this.state.number - 5 })
     }
 
-    reset() {
-        this.setState({ number: this.props.number })
+    reset = () => {
+        this.setState({number: this.props.number})
     }
 
 
 
     render() {
+        this.saveNumber()
+
         return (
             <div>
                 <h1>
                     {this.state.number}
                 </h1>
                 <button
-                    onClick={this.adding1}
+                    onClick={() => this.adding1()}
                 >
                     + 1
                 </button>
                 <button
-                    onClick={this.substract1}
+                    onClick={() => this.substract1()}
                 >
                     - 1
                 </button>
                 <button
-                    onClick={this.adding5}
+                    onClick={() => this.adding5()}
                 >
                     + 5
                 </button>
                 <button
-                    onClick={this.substract5}
+                    onClick={() => this.substract5()}
                 >
                     - 5
                 </button>
                 <button
-                    onClick={() => this.reset}
+                    onClick={() => this.reset()}
                 >
                     Reset
                 </button>
--- src/index.js
+++ src/index.js
@@ -11,5 +11,4 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-ReactDOM.render(<Counter />, document.getElementById('root'))
 
